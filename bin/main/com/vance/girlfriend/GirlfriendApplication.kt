5460c75f04573c531a70487466c6e670575431e9
VanceHo77 <gn770612@gmail.com>
2018-05-19 20:04:18 +0800

    init

---
+++ bin/main/com/vance/girlfriend/GirlfriendApplication.kt
@@ -0,0 +1,11 @@
+package com.vance.girlfriend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GirlfriendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GirlfriendApplication>(*args)
+}
---
+++ bin/main/com/vance/girlfriend/config/WebSecuirtyConfig.kt
@@ -0,0 +1,16 @@
+package com.vance.girlfriend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+public class WebSecuirtyConfig: WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.httpBasic().disable()
+    }
+
+}
