5e83719b053e219ac3dc8f4508e563a36618cab3
VanceHo77 <gn770612@gmail.com>
2018-07-15 11:44:36 +0800

    Added: search bar in header

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { IndexComponent } from './index/index.component';
@@ -100,6 +100,7 @@ export class PlunkerMaterialModule { }
     // App
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     // Router
     RouterModule,
--- src/main/webapp/src/app/header/header.component.ts
+++ src/main/webapp/src/app/header/header.component.ts
@@ -1,8 +1,16 @@
-import { HeaderService } from './header.service';
-import { Component, OnInit } from '@angular/core';
-import { AppService } from '../app.service';
-import { Topic } from '../core/interface/Topic';
-import LoggerService from '../core/logger/logger.Service';
+import { HeaderService } from './header.service'
+import { Component, OnInit } from '@angular/core'
+import { AppService } from '../app.service'
+import { Topic } from '../core/interface/Topic'
+import { FormControl } from '@angular/forms'
+import LoggerService from '../core/logger/logger.Service'
+import { Http } from '@angular/http'
+import { Subscription } from 'rxjs'
+import 'rxjs/add/operator/startWith'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/toPromise'
 
 @Component({
   selector: 'app-header',
@@ -12,30 +20,57 @@ import LoggerService from '../core/logger/logger.Service';
 })
 export class HeaderComponent implements OnInit {
 
-  public topics: Topic[];
+  public topics: Topic[]
 
-  constructor(private headerService: HeaderService, private logger:LoggerService) { }
+  searchCtrl: FormControl = new FormControl()
+  options = []
+  filteredStates: Subscription
+
+  constructor(private headerService: HeaderService, private logger: LoggerService, private http: Http) { }
+
 
   ngOnInit(): void {
-    this.topics = this.headerService.getTopics();
-    this.registerScrollFixedHeaderEvent();
+    this.topics = this.headerService.getTopics()
+    this.registerScrollFixedHeaderEvent()
     this.logger.log(this.topics)
+    this.registerSearchBarEvent()
+  }
+
+  ngOnDestroy() {
+    this.filteredStates.unsubscribe()
+  }
+
+  registerSearchBarEvent() {
+    this.filteredStates = this.searchCtrl.valueChanges
+      .startWith(null)
+      .debounceTime(5)
+      .do(val => {
+        this.http.get('https://api.icndb.com/jokes/random/3')
+          .toPromise()
+          .then(res => {
+            this.options = res.json().value
+          })
+      })
+      .subscribe()
   }
 
 
-  private registerScrollFixedHeaderEvent() {
-    window.onscroll = function () { addFixHeaderClass() };
+  registerScrollFixedHeaderEvent() {
+    window.onscroll = function () { addFixHeaderClass() }
 
-    var header = document.getElementById("header");
-    var sticky = header.offsetTop;
+    var header = document.getElementById("header")
+    var sticky = header.offsetTop
 
     function addFixHeaderClass() {
       if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
+        header.classList.add("sticky")
       } else {
-        header.classList.remove("sticky");
+        header.classList.remove("sticky")
       }
     }
+  }
+  clearSearchText() {
+    this.searchCtrl.setValue("")
   }
 }
 
