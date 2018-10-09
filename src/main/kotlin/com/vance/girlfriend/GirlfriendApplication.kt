9d293a66ef2bda322846ae510df217727152d895
Vance <gn770612@gmail.com>
2018-10-10 00:18:22 +0800

    Added: set query DB timezone to Asia/Taipei

--- src/main/kotlin/com/vance/girlfriend/GirlfriendApplication.kt
+++ src/main/kotlin/com/vance/girlfriend/GirlfriendApplication.kt
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.vance.girlfriend.model.entity.AdminUserEntity
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import java.time.LocalDateTime
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
@@ -20,8 +24,13 @@ class GirlfriendApplication : SpringBootServletInitializer(), WebMvcConfigurer {
                 .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
                 .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
     }
-}
 
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Taipei"))
+    }
+
+}
 
 fun main(args: Array<String>) {
     println("starting application...")
