be8a936d65ba9e47339c63a895b9f83507bec4ca
Vance <gn770612@gmail.com>
2018-10-09 21:53:14 +0800

    Added: db-preload.sql

---
+++ src/main/resources/db-preloaded.sql
@@ -0,0 +1,6 @@
+use girlfriend;
+
+insert into admin_user (id,account,create_time,email,modify_time,name,password) values(1,'vance',now(),'gn667226@outlook.com',now(),'vance','vance');
+
+insert into news (id,title,description,author,modify_time,create_time) values (1,'温溫診所開幕囉！！！', '慶！温溫診所開幕，診療費半價～','vance',now(),now());
+insert into news (id,title,description,author,modify_time,create_time) values (2,'新系統上線啦', '前端使用angular，後端使用kotlin及springBoot框架開發。','vance',now(),now());
